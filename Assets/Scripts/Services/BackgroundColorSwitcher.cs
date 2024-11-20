8f284ceca655e55918153fdd3378d9e806b250e7
Ceclik <yura11122345@gmail.com>
2024-11-20 22:16:23 +0300

    Added background color switching depending on location

---
+++ Assets/Scripts/Services/BackgroundColorSwitcher.cs
@@ -0,0 +1,43 @@
+﻿using Services.CharacterServices;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Services
+{
+    
+    public class BackgroundColorSwitcher : MonoBehaviour
+    {
+        [SerializeField] private Color woodsBackgroundColor;
+        [SerializeField] private Color fieldBackgroundColor;
+
+        [Space(10)] [SerializeField] private float transitionDuration;
+
+        private bool _isChanging;
+        private Color _newColor;
+        private float _elapsedTime;
+        private Image _currentColor;
+
+        private void Start()
+        {
+            _currentColor = GetComponent<Image>();
+        }
+
+        public void ChangeBackgroundColor(Location location)
+        {
+            Debug.Log($"In change color!, location: {location}");
+            _newColor = location == Location.Woods ? woodsBackgroundColor : fieldBackgroundColor;
+            _isChanging = true;
+            _elapsedTime = 0;
+        }
+
+        private void Update()
+        {
+            if (_isChanging)
+            {
+                _elapsedTime += Time.deltaTime;
+                _currentColor.color = Color.Lerp(_currentColor.color, _newColor, _elapsedTime / transitionDuration);
+                if (_elapsedTime >= transitionDuration) _isChanging = false;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Services/CharacterServices/LocationsSwitcher.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+namespace Services.CharacterServices
+{
+    public enum Location
+    {
+        Woods,
+        Field
+    }
+    
+    public class LocationsSwitcher : MonoBehaviour
+    {
+        private BackgroundColorSwitcher _colorSwitcher;
+        public Location location { get; private set; }
+
+        private void Start()
+        {
+            _colorSwitcher = GameObject.Find("Background").GetComponent<BackgroundColorSwitcher>();
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.CompareTag("Woods"))
+                location = Location.Woods;
+            else if (other.CompareTag("Field"))
+                location = Location.Field;
+            
+            _colorSwitcher.ChangeBackgroundColor(location);
+        }
+    }
+}
