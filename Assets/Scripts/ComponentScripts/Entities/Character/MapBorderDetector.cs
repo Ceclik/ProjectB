24a726fd46b935fb5400e8d2f01909034c703fed
Ceclik <yura11122345@gmail.com>
2024-11-21 23:42:37 +0300

    Added swamps around the map as a border

---
+++ Assets/Scripts/ComponentScripts/Entities/Character/MapBorderDetector.cs
@@ -0,0 +1,27 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace ComponentScripts.Entities.Character
+{
+    public class MapBorderDetector : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI actionText;
+
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            if (other.gameObject.CompareTag("SwampBorder"))
+            {
+                actionText.gameObject.SetActive(true);
+                actionText.text = "Далей ідуць непраходныя палесскія балоты! Праз іх не прабрацца!";
+            }
+        }
+
+        private void OnCollisionExit2D(Collision2D other)
+        {
+            if (other.gameObject.CompareTag("SwampBorder"))
+            {
+                actionText.gameObject.SetActive(false);
+            }
+        }
+    }
+}
