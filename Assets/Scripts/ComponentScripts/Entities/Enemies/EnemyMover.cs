099f8be256f147e889cb991e82c8a42b19a97377
Ceclik <yura11122345@gmail.com>
2025-02-13 16:12:15 +0300

    Partly added following

--- Assets/Scripts/ComponentScripts/Entities/Enemies/EnemyMover.cs
+++ Assets/Scripts/ComponentScripts/Entities/Enemies/EnemyMover.cs
@@ -12,8 +12,12 @@ namespace ComponentScripts.Entities.Enemies
         private Enemy _enemy;
         private Rigidbody2D _rigidbody;
         private bool _isMoving;
+        private bool _isStaying;
         private Animator _animator;
+        private bool _isFollowing;
+        private Transform _character;
         [SerializeField] private float onPointStayDelay;
+        [SerializeField] private float distanceToFollow;
 
         private void Start()
         {
@@ -22,10 +26,13 @@ namespace ComponentScripts.Entities.Enemies
             _points = new Transform[_pointsParent.childCount];
             _rigidbody = GetComponent<Rigidbody2D>();
             _animator = GetComponent<Animator>();
+            _character = GameObject.FindGameObjectWithTag("Player").transform;
             for (int i = 0; i < _pointsParent.childCount; i++)
                 _points[i] = _pointsParent.GetChild(i);
             _currentPointIndex = CountNextPointIndex(0);
             _isMoving = true;
+            _isFollowing = false;
+            _isStaying = false;
             _animator.SetTrigger("Move");
         }
 
@@ -41,9 +48,32 @@ namespace ComponentScripts.Entities.Enemies
 
         private void FixedUpdate()
         {
-            if (Vector2.Distance(transform.position, _points[_currentPointIndex].position) < 0.1f)
+            if (!_isFollowing && !_isStaying && Vector2.Distance(transform.position, _character.position) < distanceToFollow)
             {
                 _isMoving = false;
+                _isFollowing = true;
+                Vector2 targetPosition = _character.position;
+                Vector2 currentPosition = _rigidbody.position;
+                Vector2 direction = (targetPosition - currentPosition).normalized;
+                Vector2 movement = direction * _enemy.BaseMovingSpeed * Time.fixedDeltaTime;
+                _rigidbody.MovePosition(currentPosition + movement);
+                
+                if (movement != Vector2.zero)
+                {
+                    float angle = Mathf.Atan2(movement.y, movement.x) * Mathf.Rad2Deg - 90;
+                    _rigidbody.rotation = angle;
+                }
+            }
+            else if (_isFollowing && Vector2.Distance(transform.position, _character.position) > distanceToFollow)
+            {
+                _isMoving = true;
+                _isFollowing = false;
+            }
+            
+            if (!_isFollowing && Vector2.Distance(transform.position, _points[_currentPointIndex].position) < 0.1f)
+            {
+                _isMoving = false;
+                _isStaying = true;
                 _rigidbody.freezeRotation = true;
                 _rigidbody.isKinematic = true;
                 _animator.ResetTrigger("Move");
@@ -77,6 +107,7 @@ namespace ComponentScripts.Entities.Enemies
             _animator.SetTrigger("Move");
             _animator.ResetTrigger("Stop");
             _isMoving = true;
+            _isStaying = false;
         }
     }
 }
